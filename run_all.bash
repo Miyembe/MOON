87d1ec96ffe648430b1e567b1872935340bc0e9a
miyembe <respct11@gmail.com>
2022-12-28 13:24:58 +0000

    main_drl_simple updated. envs initialisation updated

--- run_all.bash
+++ run_all.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 python3 main.py --dataset=cifar10     --model=simple-cnn     --alg=moon     --lr=0.01     --mu=5     --epochs=10     --comm_round=10     --n_parties=10     --partition=noniid     --beta=0.5     --logdir='./logs/'
 python3 main.py --dataset=cifar10     --model=simple-cnn     --alg=fedavg     --lr=0.01     --mu=5     --epochs=10     --comm_round=10     --n_parties=10     --partition=noniid     --beta=0.5     --logdir='./logs/'
